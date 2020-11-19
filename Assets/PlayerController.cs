16a1f9f377ea4a278bc8bad8710c83f7063d39b1
Søren Kurgan Jacobsen <kurgan@kurgan.dk>
2020-11-19 18:44:01 +0100

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+
+public class PlayerController : NetworkBehaviour
+{
+
+    public float rotationsSpeed = 2f;
+    private Rigidbody2D rbd;
+    private Transform engineFlameTransform;
+    private GameObject cameraLink;
+    private int health = 100;
+    public GameObject bulletPrefab;
+    public int score;
+    public Text scoreText;
+    public GameObject nozzleLink;
+    public AudioSource engineSound;
+    public AudioSource hitSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        rbd = GetComponent<Rigidbody2D>();
+
+        // only do this for my own player/ship
+        if (isLocalPlayer)
+        {
+            score = 0;
+            // make my own ship red
+            GetComponent<SpriteRenderer>().color = Color.red;
+            // find local camera
+            cameraLink = GameObject.Find("CameraHolder");
+            // find my engine flame
+            engineFlameTransform = this.gameObject.transform.GetChild(0);
+        }
+        takeDMG();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // only do this for my own player/ship
+        if (isLocalPlayer)
+        {
+            // get keyboard input wasd + space
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                // call server bulletspawner
+                CmdFireBullet();
+            }
+
+            // move and rotate ship
+            rbd.AddForce(transform.up * y);
+            engineFlameTransform.localScale = (Vector3.one) * (y+1f) * 0.3f;
+            engineSound.volume = Mathf.Abs(y);
+            rbd.rotation -= x * rotationsSpeed;
+
+            // update camera
+            cameraLink.transform.position = transform.position;
+        }
+
+
+    }
+    [Command]
+    void CmdFireBullet()
+    {
+        // instantiate bullet
+        var bullet= Instantiate(bulletPrefab, nozzleLink.transform.position, transform.rotation);
+
+        // set the speed
+        //bullet.GetComponent<Rigidbody2D>().velocity = transform.up * 6;
+
+        // spawn on network
+        NetworkServer.Spawn(bullet);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+
+    public void takeDMG()
+    {
+        Debug.Log("take dmg");
+        hitSound.Play();
+        health--;
+        scoreText.text = health.ToString();
+    }
+
+}
---
+++ Assets/bulletScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class bulletScript : NetworkBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(transform.up * 6);
+
+        GetComponent<Rigidbody2D>().velocity = transform.up * 6;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var hit = collision.gameObject;
+        if (hit.tag == "Player")
+        {
+            var PC = hit.GetComponent<PlayerController>();
+            Debug.Log("deal dmg");
+            PC.takeDMG();
+        }
+    }
+}
